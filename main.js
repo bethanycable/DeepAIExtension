5a4f06bf78cd342db47c3b54092ae2d8c8fa7a6b
Bethany Mattern <bethany.a.cable@gmail.com>
2022-12-01 00:26:01 -0500

    Add eventlistener on generate button and update input field

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const deepai = require('deepai');
+// deepai.setApiKey('');
 
-deepai.setApiKey('API_KEY');
+let randomizedImage = document.getElementById('randomized-image');
+const generateButton = document.getElementById('generate-button');
+const inputText = document.getElementById('search-box');
 
+generateButton.addEventListener('click', () => {
+  let testInput = inputText.value;
+  inputText.value = '';
+  // async function aiGenerator() {
+  //   const response = await deepai.callStandardApi('stable-diffusion', {
+  //     text: testInput,
+  //   });
+  //   //   const data = await response.json();
+  // //   return data;
+  // }
+  //aiGenerator();
 
-async function aiGenerator() {
-    const response = await deepai.callStandardApi("stable-diffusion",  {
-        text: document.getElementById('imageGenerator');
-    })
-}
+  const data = {
+    id: '08badc2f-43ca-46cd-acf5-ece84cdc22d0',
+    output_url:
+      'https://api.deepai.org/job-view-file/08badc2f-43ca-46cd-acf5-ece84cdc22d0/outputs/output.jpg',
+  };
 
+  randomizedImage.src = data.output_url;
+});
